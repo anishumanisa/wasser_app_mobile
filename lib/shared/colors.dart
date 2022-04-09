790da4c5f63d4571aedc9fdc2fc6b57b0bba3af5
DESKTOP-1J3CHLM\ASUS <anis.humanisa1403@gmail.com>
2022-04-09 16:33:35 +0700

    add color

---
+++ lib/shared/colors.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+const colorPrimary = Color(0xFF5077DF);
+
+///natural color
+const kNeutral1000 = Color(0xFF000000);
+const kNeutral900 = Color(0xFF181818);
+const kNeutral800 = Color(0xFF424242);
+const kNeutral700 = Color(0xFF616161);
+const kNeutral600 = Color(0xFF757575);
+const kNeutral500 = Color(0xFF9E9E9E);
+const kNeutral400 = Color(0xFFBDBDBD);
+const kNeutral300 = Color(0xFFE0E0E0);
+const kNeutral200 = Color(0xFFEEEEEE);
+const kNeutral100 = Color(0xFFF5F5F5);
+const kNeutral50 = Color(0xFFFAFAFA);
+const kNeutral0 = Color(0xFFFFFFFF);
+
+const colorWarningHeavy = Color(0xFFEF6C00);
+const colorWarningHigh = Color(0xFFFFD54F);
+const colorWarningMedium = Color(0xFFFFE082);
+const colorWarningLow = Color(0xFFFFECB3);
+const colorWarningHeavyOpacity = Color(0x1AEF6C00);
+
+const colorInformationHeavy = Color(0xFF1565C0);
+const colorInformationHigh = Color(0xFF2196F3);
+const colorInformationMedium = Color(0xFF64B5F6);
+const colorInformationLow = Color(0xFFBBDEFB);
+
+const colorSuccessHeavy = Color(0xFF2E7D32);
+const colorSuccessHigh = Color(0xFF4CAF50);
+const colorSuccessMedium = Color(0xFF81C784);
+const colorSuccessLow = Color(0xFFC8E6C9);
+
+const colorErrorHeavy = Color(0xFFB71C1C);
+const colorErrorHigh = Color(0xFFF44336);
+const colorErrorMedium = Color(0xFFE57373);
+const colorErrorLow = Color(0xFFFFCDD2);
+
+const colorBrokenHeavy = Color(0xFF6A1B9A);
+const colorBrokenHigh = Color(0xFFAB47BC);
+const colorBrokenMedium = Color(0xFFCE93D8);
+const colorBrokenLow = Color(0xFFE1BEE7);
