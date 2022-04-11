a37bc2f1655b7f65c373c9d2b4512a531693ed2e
DESKTOP-1J3CHLM\ASUS <anis.humanisa1403@gmail.com>
2022-04-11 16:32:52 +0700

    add profile

---
+++ lib/ui/pages/main/main_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {}
---
+++ lib/ui/pages/main/widget/bottom_navbar.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigationBarMw extends StatefulWidget {
+  final PageController pageController;
+  static final lastIndex = 3;
+
+  BottomNavigationBarMw({required this.pageController});
+
+  @override
+  State<BottomNavigationBarMw> createState() => _BottomNavigationBarMwState();
+}
+
+class _BottomNavigationBarMwState extends State<BottomNavigationBarMw> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: _currentIndex,
+      onTap: (index) {
+        widget.pageController.jumpToPage(index);
+        setState(() {
+          _currentIndex = index;
+        });
+      },
+      type: BottomNavigationBarType.fixed,
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.api), label: "Lorem"),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_sharp), label: "Lorem"),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_sharp), label: "Lorem"),
+      ],
+    );
+  }
+}
