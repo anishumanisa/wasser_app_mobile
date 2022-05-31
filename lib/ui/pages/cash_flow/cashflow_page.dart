3b4013bcfa3430edd9557c199bbc6b6fa80a42d2
tedialamsyah <tedi@machtwatch.co.id>
2022-05-31 14:03:57 +0700

    remove unused code

--- lib/ui/pages/cash_flow/cashflow_page.dart
+++ lib/ui/pages/cash_flow/cashflow_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:wasser_app/shared/colors.dart';
 
 class CashFlowPage extends StatefulWidget {
   const CashFlowPage({Key? key}) : super(key: key);
